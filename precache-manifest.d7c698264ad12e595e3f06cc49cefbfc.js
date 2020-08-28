338499a034ce670f9a6f4faffe5f5f9eff491da8
Thiago Bôa <thiagoboat@gmail.com>
2020-08-28 11:13:36 -0300

    Updates

---
+++ precache-manifest.d7c698264ad12e595e3f06cc49cefbfc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237e01feb68db0709a70d609726f7b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "830fba46a45038bc2519",
+    "url": "/static/css/2.0971e70a.chunk.css"
+  },
+  {
+    "revision": "830fba46a45038bc2519",
+    "url": "/static/js/2.4893d1e9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.4893d1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5dfa84b1f6f6653951f",
+    "url": "/static/js/main.f716d55b.chunk.js"
+  },
+  {
+    "revision": "8761b8e4695d0ca226a7",
+    "url": "/static/js/runtime-main.55252a19.js"
+  },
+  {
+    "revision": "43cd998eefadd5895b32f5f845d48789",
+    "url": "/static/media/TB_logo.43cd998e.svg"
+  },
+  {
+    "revision": "67781215a4875290252282cb1760ee10",
+    "url": "/static/media/chiave_logo.67781215.png"
+  },
+  {
+    "revision": "bbc4e13581ca75c1e1fcfebff29d3aeb",
+    "url": "/static/media/gfx_logo.bbc4e135.png"
+  },
+  {
+    "revision": "c9a5f7e5ff934c659fd146a1a2299726",
+    "url": "/static/media/thiago_boa.c9a5f7e5.png"
+  }
+]);
